7d782921b2c23395cdb7a86c87c154506089445b
Mohammed Mwijaa <mm.mwijaa@gmail.com>
2022-10-27 21:05:12 +0300

    Implement login test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,23 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+from settings import Settings
+
+
+config = Settings()
+
+@pytest.fixture(scope="module")
+def driver():
+    driver_options = webdriver.ChromeOptions()
+    driver_options.headless = config.ci_env
+    driver_options.set_capability("loggingPrefs", dict(performance="ALL"))
+
+    _driver = webdriver.Chrome(options=driver_options)
+    yield _driver
+
+    _driver.quit()
+
+@pytest.fixture(scope="module")
+def cfg() -> Settings:
+    return config
